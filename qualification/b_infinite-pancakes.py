097b13aa0f1c59874c3046807dd6b22c4b0170e4
Amy Xiao <amyyuexiao@gmail.com>
2015-04-20 12:16:05 -0400

    Divide input into pancakesPiles

--- qualification/b_infinite-pancakes.py
+++ qualification/b_infinite-pancakes.py
@@ -12,13 +12,48 @@ while True:
     
 #print("Here is your input:")
 
-cases = input[0]
-pancakesData = input[1:]
+try:
+    cases = input[0]
+    pancakesData = input[1:]
+except:
+    print "Invalid lists"
 
 i = 0
 
 while i+2 <= len(pancakesData):
-    case = pancakesData[i:i+2]
+    case = pancakesData[i].split() + pancakesData[i+1].split()
     i += 1
     print(case)
+
+case.sort()
+
+numPeople = case[0]
+pancakePiles = case[1:]
+
+for i in range(len(pancakePiles)):
+#    while pancakePiles[i+1] is not None:
+        if (int(pancakePiles[i]) + 2) <= int(pancakePiles[i+1]):
+    	    max = int(pancakePiles[i+1])
+
+print(max)
+
+#largestPile = case[1]
+#smallPile = 0
+
+#for pile in range(len(pancakePiles)):
+        
+#if int(pancakePiles[pile]) >= (int(largestPile) + 2):
+#            smallPile = int(largestPile)
+#            largestPile = int(pancakePiles[pile])
+#    print(largestPile)
+#    print(smallPile)
+    
+#    [x for x in pancakePiles
+   # largePileCount = 0
+    
+    
+#for pile in:
+#        while int(largestPile) - 2 not in a:
+#	     if int(largestPile) - 1 in a:
+#	         largePileCount += 1
  
